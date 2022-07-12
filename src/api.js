9e6bf50da78c40b5108f881749365fa5a2b2fdb2
wjdgus1122 <qodqodantk@gmail.com>
2022-07-12 13:47:01 +0900

    tv episode api setting

--- src/api.js
+++ src/api.js
@@ -23,4 +23,5 @@ export const apiData = {
   tv_Detail: (id) => baseapi.get(`/tv/${id}`),
   mv_video: (id) => baseapi.get(`/movie/${id}/videos`),
   tv_video: (id) => baseapi.get(`/tv/${id}/videos`),
+  tv_episode_detail: (id, season) => baseapi.get(`/tv/${id}/season/${season}`),
 };
