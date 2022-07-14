3346a5447c8154ed9bb374214092e1d73fd58c02
wjdgus1122 <qodqodantk@gmail.com>
2022-07-14 23:57:23 +0900

    Search setting2

--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -1,5 +1,7 @@
-import { useEffect } from "react";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
 import styled from "styled-components";
+import { apiData } from "../../api";
 import { mainStyle } from "../../styles/Globalstyle";
 
 const SearchWrap = styled.form`
@@ -23,10 +25,54 @@ const SearchInput = styled.input`
 `;
 
 export const Search = () => {
+  const [sh, setSh] = useState();
+  const [loading, setLoading] = useState();
+  const {
+    register,
+    handleSubmit,
+    setError,
+    getValues,
+    formState: { errors, isValid },
+    clearErrors,
+  } = useForm({
+    mode: "onChange",
+  });
+  const onsubmit = async () => {
+    const { search: term } = getValues;
+    setLoading(true);
+    try {
+      const {
+        data: { results: movieresults },
+      } = await apiData.mv_search(term);
+      const {
+        data: { results: tvresults },
+      } = await apiData.tv_search(term);
+      if (movieresults.length && tvresults.length <= 0) {
+        setError("notsearch", {
+          message: "검색결과를 찾을수 없습니다.",
+        });
+      } else {
+        setSh(movieresults, tvresults);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <>
-      <SearchWrap>
-        <SearchInput type="text" placeholder="검색어를 입력하세요." />
+      <SearchWrap onSubmit={handleSubmit(onsubmit)}>
+        <SearchInput
+          {...register("search", {
+            required: "검색어는 반드시 입력해야합니다.",
+            onChange() {
+              clearErrors("notsearch");
+            },
+          })}
+          type="text"
+          placeholder="검색어를 입력하세요."
+        />
+        {errors?.search?.message}
+        {errors?.notsearch?.message}
       </SearchWrap>
     </>
   );
